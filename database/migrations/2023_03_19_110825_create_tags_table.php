bc8832588c5d4222a64593002128e4bf8e85c66a
San-Francisco1 <ryazanov-alex.com@mail.ru>
2023-03-19 19:47:50 +0300

    edited migrations

--- database/migrations/2023_03_19_110825_create_tags_table.php
+++ database/migrations/2023_03_19_110825_create_tags_table.php
@@ -15,8 +15,7 @@ class CreateTagsTable extends Migration
     {
         Schema::create('tags', function (Blueprint $table) {
             $table->id();
-            $table->string('label');
-            //$table->timestamps();
+            $table->string('label')->unique();
         });
     }
 
