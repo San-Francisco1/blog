bd92249ac0f7fce563c1b8c1fa24318d33f80df2
San-Francisco1 <ryazanov-alex.com@mail.ru>
2023-03-21 12:28:20 +0300

    Refactor article page

--- resources/views/app/article/show.blade.php
+++ resources/views/app/article/show.blade.php
@@ -17,6 +17,10 @@
             </p>
             <p class="card-text">{{$article->body}}</p>
             <p>Опубликовано:  <i>{{$article->publishedAtForHumans()}}</i> </p>
+            <div class="mt-3">
+                <span class="badge bg-primary">{{$article->state->likes}} <i class="far fa-thumbs-up"></i> </span>
+                <span class="badge bg-danger">{{$article->state->views}} <i class="far fa-eye"></i> </span>
+            </div>
         </div>
 
     </div>
@@ -38,7 +42,7 @@
                 <div class="my_toast showing" style="width: 100%;">
                     <div class="my_toast-header">
                         <img src="https://upload.wikimedia.org/wikipedia/commons/5/59/User-avatar.svg" class="rounded me-2" alt="...">
-                        <strong class="me-auto">{{$comment->subject}}}</strong>
+                        <strong class="me-auto">{{$comment->subject}}</strong>
                         <small class="text-muted">{{$comment->createdAtForHumans()}}</small>
                     </div>
                     <div class="my_toast-body">
@@ -51,5 +55,5 @@
 </div>
 @endsection
 @section('vue')
-
+    <script src=" {{ mix('/js/app.js') }} "></script>
 @endsection
