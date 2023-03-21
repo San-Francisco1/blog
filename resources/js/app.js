db2653be547c48431f65520fe3a164cb8794773a
San-Francisco1 <ryazanov-alex.com@mail.ru>
2023-03-21 12:59:30 +0300

    Import vuex

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,13 @@ window._ = require('lodash');
 window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-require('./bootstrap');
-
 window.Vue = require('vue').default;
+
+import store from './store'
 
 Vue.component('article-component', require('./components/ArticleComponent.vue').default);
 
 const app = new Vue({
+    store,
     el: '#app',
 });
